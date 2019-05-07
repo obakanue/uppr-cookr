d260186b9c817bc3bc5c01800048ab896529aab5
Astrid Jansson <ascii123@github.com>
2019-05-07 16:01:39 +0200

    arrows added, proximity sensor adds minutes to the timer

--- app/src/main/java/com/example/cookr/TimerActivity.java
+++ app/src/main/java/com/example/cookr/TimerActivity.java
@@ -2,22 +2,60 @@ package com.example.cookr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.TextView;
 
 
-public class TimerActivity extends AppCompatActivity {
+public class TimerActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager mSensorManager;
+    private Sensor mProximity;
+    private static final int SENSOR_SENSITIVITY = 4;
     TextView textView;
     public int counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
         textView = (TextView)findViewById(R.id.tickingTimer);
         counter = 0;
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mProximity, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+            if (event.values[0] >= -SENSOR_SENSITIVITY && event.values[0] <= SENSOR_SENSITIVITY) {
+                counter += 60;
+                textView.setText(String.valueOf(counter));
+            }
+        }
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
     public void up(View v) {
         counter ++;
         textView.setText(String.valueOf(counter));
@@ -31,6 +69,7 @@ public class TimerActivity extends AppCompatActivity {
         }
     }
     public void startTimer(View v) {
+        textView.setTextColor(Color.parseColor("#725F42"));
         new CountDownTimer(counter*1000, 1000){
             public void onTick(long millisUntilFinished){
                 textView.setText(String.valueOf(counter));
@@ -38,8 +77,11 @@ public class TimerActivity extends AppCompatActivity {
             }
             public  void onFinish(){
                 textView.setText("FINISH!!");
+                textView.setTextColor(Color.parseColor("#474338"));
             }
         }.start();
 
     }
 }
+
+
