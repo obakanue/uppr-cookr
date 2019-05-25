83aff2e54bc5f63d1027e92ae3aa92a30e29256b
Sofi <sofi.flinck@gmail.com>
2019-05-25 21:07:19 +0200

    pancake list item clickable

--- app/src/main/java/com/example/cookr/RecipeActivity.java
+++ app/src/main/java/com/example/cookr/RecipeActivity.java
@@ -1,12 +1,20 @@
 package com.example.cookr;
 
+
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +24,7 @@ public class RecipeActivity extends AppCompatActivity implements SensorEventList
     private Sensor mProximity;
     private static final int SENSOR_SENSITIVITY = 4;
     private TextView distance;
+    String recipeArray[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,24 @@ public class RecipeActivity extends AppCompatActivity implements SensorEventList
         mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
         distance = (TextView) findViewById(R.id.distance);
 
+        ListView recipe_list = findViewById(R.id.listview);
+        recipe_list.setOnItemClickListener(listener());
+        ListAdapter adapter= recipe_list.getAdapter();
+        recipe_list.setAdapter(adapter);
+        recipeArray = getResources().getStringArray(R.array.list_items);
+
+    }
+
+    private OnItemClickListener listener(){
+        return new OnItemClickListener(){
+            public void onItemClick(AdapterView<?> p, View v, int x, long id){
+                String recipe = p.getItemAtPosition(x).toString();
+                if("PANCAKES".equals(recipe)){
+                    Intent intent = new Intent(RecipeActivity.this, RecipeOverview.class);
+                    startActivity(intent);
+                }
+            }
+        };
     }
 
     @Override
